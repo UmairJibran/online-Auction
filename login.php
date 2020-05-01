e9dfe64937172ae33760d477ef7ced044528c917
umairjibran <umairjibran7@gmail.com>
2020-05-02 04:01:09 +0500

    added sign up functionality

--- login.php
+++ login.php
@@ -21,23 +21,24 @@
     <div class="row">
         <div class="col col-1"></div>
         <div class="col col-6">
+            <!--Sign up Starts Here -->
             <center>
                 <h1>Sign Up</h1>
             </center>
             <form action="#" method="post">
                 <center>
                     <div class="md-form">
-                        <input required type="text" id="FirstName" placeholder="First Name">
-                        <input required type="email" id="email" placeholder="Email Address" style="float: right;">
+                        <input required type="text" name="FirstName" placeholder="First Name">
+                        <input required type="email" name="email" placeholder="Email Address" style="float: right;">
                         <br><br>
-                        <input required type="text" id="LastName" placeholder="Last Name">
-                        <input required type="password" id="password" placeholder="Your Password" style="float: right;">
+                        <input required type="text" name="LastName" placeholder="Last Name">
+                        <input required type="password" name="password" placeholder="Your Password" style="float: right;">
                         <br><br>
-                        <input required type="number" id="Contact" placeholder="Contact">
-                        <input type="submit" value="Sign Up" class="btn btn-primary"
+                        <input required type="number" name="Contact" placeholder="Contact">
+                        <input type="submit" value="Sign Up" class="btn btn-primary" name = 'signUp'
                             style="float: right; margin-left: 60px;">
                         <br><br>
-                        <select required name="payment" id="payment" class="custom-select">
+                        <select required name="payment" name="payment" class="custom-select">
                             <option value="easypaisa">EasyPaisa</option>
                             <option value="cashOnDelivery">Cash on Delivery</option>
                         </select>
@@ -48,13 +49,14 @@
         <div class="col col-1" style="border-right: 1px solid #7070702F"></div>
         <div class="col col-1"></div>
         <div class="col col-2">
+            <!--Login Starts Here -->
             <center>
                 <h1>Log In</h1>
                 <form action="#" method="post">
                     <div class="md-form">
-                        <input required type="email" name="Email" id="email" placeholder="Email Address">
+                        <input required type="email" name="Email" placeholder="Email Address">
                         <br><br>
-                        <input required type="password" name="password" id="password" placeholder="Your Password">
+                        <input required type="password" name="password" placeholder="Your Password">
                         <br><br>
                         <input type="submit" name='login' value="Log In" class="btn btn-primary" style="float: right;">
                     </div>
@@ -82,6 +84,40 @@
             }
         }else{
             echo "<br><br><center>Please Double Check your Credential</center>";
+        }
+    }
+    if(isset($_POST['signUp'])){
+        $user_first_name = $_POST['FirstName'];
+        $user_last_name = $_POST['LastName'];
+        $user_contact = $_POST['Contact'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $payment = $_POST['payment'];
+        $query = "SELECT *  FROM `auction_user` WHERE `user_email` = '$email' OR `user_contact` = '$user_contact'";
+        $result = $conn->query($query);
+        $rows = $result->num_rows;
+        if($rows >= 1){
+            print "<br><br><center>There Exists a User with same email or Password</center>" ;
+        }else{
+            if($payment == 'easypaisa') $payment = 'esp';
+            else $payment = 'cod';
+            $query = "INSERT INTO `auction_user` (`user_first_name`, `user_last_name`, `user_email`, `user_contact`, `user_password`, `payment`) VALUES ('$user_first_name', '$user_last_name', '$email', '$user_contact', '$password', '$payment'); ";
+            $result = $conn->query($query);
+            if(!$result){
+                echo '<br><br><center>We Encountered a Problem, please try again</center>';
+            }else{
+                $query = "SELECT * FROM `auction_user` WHERE `user_email` = '$email'";
+                $result = $conn->query($query);
+                $rows = $result->num_rows;
+                if($rows == 1){
+                    while ($data = $result->fetch_assoc())
+                        $userID =   $data['user_id'];
+                }             
+                setcookie('user_id', $userID, time() + 3600); //we want the user Logged in for one hour
+                setcookie('user_first_name', $user_first_name, time() + 3600);
+                setcookie('user_last_name', $user_last_name, time() + 3600);
+                header('location:index.php');
+            }
         }
     }
     
