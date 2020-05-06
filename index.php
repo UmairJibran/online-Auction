067f5c071139b073351bbded31e9b426493a917b
umairjibran <umairjibran7@gmail.com>
2020-05-06 05:49:48 +0500

    developed front-end and back-end for home page

--- index.php
+++ index.php
@@ -1,13 +1,6 @@
 <?php
     require_once('connection.php');
-    if(!isset($_COOKIE['user_id'])){
-        header('location:login.php',false);
-        die;
-    }
-    if(isset($_POST['signOut'])){
-        setcookie('user_id',$_COOKIE['user_id'],time() - 1);
-        header('location:login.php');
-    }
+    include_once('navigation.php');
 ?>
 
 <html>
@@ -15,19 +8,43 @@
         <title>Auction - Home</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
         <link href="https://fonts.googleapis.com/css2?family=Sulphur+Point:wght@400;700&display=swap" rel="stylesheet">
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.16.0/css/mdb.min.css" rel="stylesheet">
         <link rel="stylesheet" href="styles/master.css">
     </head>
     <body>
         <div class="container">
-            <div class="jumbotron">
-                <center>
-                    <h1>Welcom <?php echo "{$_COOKIE['user_first_name']}" . " " . "{$_COOKIE['user_last_name']}" ?></h1>
-                </center>
+            <div class="body">
+                <div class="row row-cols-1 row-cols-md-4">
+                    <?php
+                        $query = "SELECT `image`,`item_name`,`item_id`,`init_bid`,`current_bid` FROM `auction_item`";
+                        $result = $conn->query($query);
+                        $rows = $result->num_rows;
+                        if($rows >= 1){
+                            while ($data = $result->fetch_assoc()){
+                                $name = $data['item_name'];
+                                $image = $data['image'];
+                                $price = 0;
+                                $id = $data['item_id'];
+                                if(!$data['current_bid'] == 0){
+                                    $price = $data['current_bid'];
+                                }else{
+                                    $price = $data['init_bid'];
+                                }
+                                echo'<a href="singleItem.php?itemId=';echo $id;echo'&itemName=';echo $name;echo'" class="itemCard"><div class="col mb-4">
+                                    <div class="card">
+                                        <img src="';echo $image;echo '" class="card-img-top" alt="';echo $name;echo'" height="300">
+                                        <div class="card-body">
+                                            <h5 class="card-title">';echo $name;echo'</h5>
+                                            <h5 style="float:right;">Rs. ';echo $price;echo '</h5>
+                                        </div>
+                                    </div>
+                                </div></a>';
+                            }
+                        }
+                    ?>
+                </div>
             </div>
-            <form method='POST'>
-                <input type="submit" value="Sign Out" class="btn btn-primary" name='signOut' style='float:right;'>
-            </form>
         </div>
     </body>
 </html>
+
+<!-- INSERT INTO `auction_item` (`item_id`, `item_name`, `item_type`, `init_bid`, `current_bid`, `due_date`, `post_date`, `category`, `item_condition`, `location`, `description`, `image`, `item_poster`) VALUES (NULL, 'Sofa', '', '5000', '0', '2020-05-21', '2020-05-06', 'Furniture', 'old', 'Peshawar', 'This is a very good sofa, you can sit you can lay down, watch TV or give us 4GPA when sitting on it :)', '', '1'); -->
---
+++ navigation.php
@@ -0,0 +1,43 @@
+<?php
+    if(isset($_POST['signOut'])){
+        setcookie('user_id',$_COOKIE['user_id'],time() - 1);
+        header('location:login.php');
+    }
+    if(!isset($_COOKIE['user_id'])){
+        header('location:login.php',false);
+        die;
+    }
+?>
+
+<html>
+    <body>
+        <div class="jumbotron">
+            <div class="row">
+                <div class="col col-3">
+                    <h1 style='float:left;'><a class="text-primary" href="index.php">Home</a></h1>
+                </div>
+                <div class="col col-6">
+                <form method='POST' action="items.php">
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <div class="input-group-text">
+                                <img src="assets/search-solid.svg" alt="Search Icon" height='15' width='15'>
+                            </div>
+                        </div>
+                        <input type="text" name="itemName" required placeholder='Search an Item' class='form-control'>
+                        <input type="submit" value="Search" class='btn btn-outline-primary' style='float:right;'>
+                    </div>
+                </form>
+                </div>
+                <div class="col col-3">
+                    <div style = "float:right;">
+                        <h5>Hello, <?php echo"{$_COOKIE['user_first_name']}"; ?></h5>
+                        <form method='POST'>
+                            <input type="submit" value="Sign Out" class="btn btn-outline-primary" name='signOut' style='float:right;'>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
